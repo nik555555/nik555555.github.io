2ddee3618b9effe3aef11d6cf4949a09864c779a
nik555555 <76729095+nik555555@users.noreply.github.com>
2020-12-29 20:34:35 +1000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', checkDevice);
 
 function checkDevice(event){
     let viberLink = document.getElementById('viber_link');
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    if (!(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
         /* viberLink.setAttribute('href', 'add?number=79170382332'); */
         alert('ЭСМИнец');
     }
